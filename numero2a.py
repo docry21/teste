e8ad59457499363e08a37cab1c4baddb4013d5fc
docry21 <127063971+docry21@users.noreply.github.com>
2023-03-05 23:33:33 -0300

    Create numero2a.py

---
+++ numero2a.py
@@ -0,0 +1,10 @@
+# Definindo a sequência como uma lista
+numeros = [1, 3, 5, 7, 9]
+
+# Verificando o próximo número ímpar na sequência
+numero2 = numeros[-1] + 2
+
+if numero2 == 11:
+    print("O próximo número na sequência é 11.")
+else:
+    print("A lógica da sequência está incorreta.")
