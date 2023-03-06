4a4dd10952248dddac38ed55b9b5e7df62d271dc
docry21 <127063971+docry21@users.noreply.github.com>
2023-03-05 23:38:54 -0300

    Create numero2d.py

---
+++ numero2d.py
@@ -0,0 +1,7 @@
+# Definindo a sequência como uma lista
+lista = [4, 16, 36, 64]
+
+# Verificando o próximo número na sequência
+numero = (len(lista) + 2) ** 2
+
+print("O próximo número na sequência é:", proximo)
