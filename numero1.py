aea7f3a7274dbb616172f928a04154fd0ead018d
docry21 <127063971+docry21@users.noreply.github.com>
2023-03-05 23:32:13 -0300

    Create numero1.py

---
+++ numero1.py
@@ -0,0 +1,17 @@
+def verifica(n):
+    a, b = 0, 1
+    while b < n:
+        a, b = b, a + b
+    if b == n:
+        return True
+    else:
+        return False
+
+# Solicita um número ao usuário
+num = int(input("Digite um número: "))
+
+# Verifica se o número pertence à sequência de Fibonacci
+if verifica(num):
+    print(f"{num} pertence à sequência de Fibonacci.")
+else:
+    print(f"{num} não pertence à sequência de Fibonacci.")
