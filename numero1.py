3fe3c954c6a729545fe7729243b98b6b7bbaa10a
docry21 <127063971+docry21@users.noreply.github.com>
2023-03-05 23:54:43 -0300

    Create numero1.py

---
+++ numero1.py
@@ -0,0 +1,17 @@
+int 
+INDICE = 13, 
+SOMA = 0, 
+K = 0;
+
+K < INDICE 
+Como K menor que indice que é 13
+{
+
+K = K + 1;
+K = 0 + 1 = 1
+SOMA = SOMA + K;
+SOMA = 0 + 1 = 1
+}
+
+imprimir(SOMA);
+Vai imprimir o valor 1
