778fa5554f030d57de6b21fa0baa2b44920df9f5
docry21 <127063971+docry21@users.noreply.github.com>
2023-03-05 23:40:20 -0300

    Create numero2e.py

---
+++ numero2e.py
@@ -0,0 +1,7 @@
+# Definindo a sequência como uma lista
+sequencia = [1, 1, 2, 3, 5, 8]
+
+# Verificando
+proximo = sequencia[-1] + sequencia[-2]
+
+print("O próximo número na sequência é:", proximo)
